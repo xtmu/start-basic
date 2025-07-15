51e1b6d2cdda5b8163fdb5b1e61441520b1f024a
xtmu <xtmu21@gmail.com>
2025-07-16 03:42:37 +0800

    update

--- src/routes/api/users.ts
+++ src/routes/api/users.ts
@@ -1,35 +1,36 @@
-import { createServerFileRoute } from '@tanstack/react-start/server'
-import { getRequestHeaders } from '@tanstack/react-start/server'
-import { createMiddleware, json } from '@tanstack/react-start'
-import type { User } from '~/utils/users'
+import { createServerFileRoute } from "@tanstack/react-start/server";
+import { getRequestHeaders } from "@tanstack/react-start/server";
+import { createMiddleware, json } from "@tanstack/react-start";
+import type { User } from "~/utils/users";
+import { z } from "zod";
 
-const userLoggerMiddleware = createMiddleware({ type: 'request' }).server(
+const userLoggerMiddleware = createMiddleware({ type: "request" }).server(
   async ({ next, request }) => {
-    console.info('In: /users')
-    console.info('Request Headers:', getRequestHeaders())
-    const result = await next()
-    result.response.headers.set('x-users', 'true')
-    console.info('Out: /users')
-    return result
+    console.info("In: /users");
+    console.info("Request Headers:", getRequestHeaders());
+    const result = await next();
+    result.response.headers.set("x-users", "true");
+    console.info("Out: /users");
+    return result;
   },
-)
+);
 
-const testParentMiddleware = createMiddleware({ type: 'request' }).server(
+const testParentMiddleware = createMiddleware({ type: "request" }).server(
   async ({ next, request }) => {
-    console.info('In: testParentMiddleware')
-    const result = await next()
-    result.response.headers.set('x-test-parent', 'true')
-    console.info('Out: testParentMiddleware')
-    return result
+    console.info("In: testParentMiddleware");
+    const result = await next({ context: { testParent: true } });
+    result.response.headers.set("x-test-parent", "true");
+    console.info("Out: testParentMiddleware");
+    return result;
   },
-)
+);
 
-const testMiddleware = createMiddleware({ type: 'request' })
+const testMiddleware = createMiddleware({ type: "request" })
   .middleware([testParentMiddleware])
   .server(async ({ next, request }) => {
-    console.info('In: testMiddleware')
-    const result = await next()
-    result.response.headers.set('x-test', 'true')
+    console.info("In: testMiddleware");
+    const result = await next({ context: { test: true } });
+    result.response.headers.set("x-test", "true");
 
     // if (Math.random() > 0.5) {
     //   throw new Response(null, {
@@ -38,25 +39,26 @@ const testMiddleware = createMiddleware({ type: 'request' })
     //   })
     // }
 
-    console.info('Out: testMiddleware')
-    return result
-  })
+    console.info("Out: testMiddleware");
+    return result;
+  });
 
-export const ServerRoute = createServerFileRoute('/api/users')
-  .middleware([testMiddleware, userLoggerMiddleware, testParentMiddleware])
+export const ServerRoute = createServerFileRoute("/api/users")
+  .middleware([userLoggerMiddleware, testParentMiddleware, testMiddleware])
   .methods({
-    GET: async ({ request }) => {
-      console.info('GET /api/users @', request.url)
-      console.info('Fetching users... @', request.url)
-      const res = await fetch('https://jsonplaceholder.typicode.com/users')
+    GET: async ({ request, context }) => {
+      console.info("GET /api/users @", request.url);
+      console.info("Fetching users... @", request.url);
+      console.info("Context:", context); // context inferred type is { testParent: boolean; test: boolean; }, but actual context is always replaced by the last middleware's context.
+      const res = await fetch("https://jsonplaceholder.typicode.com/users");
       if (!res.ok) {
-        throw new Error('Failed to fetch users')
+        throw new Error("Failed to fetch users");
       }
 
-      const data = (await res.json()) as Array<User>
+      const data = (await res.json()) as Array<User>;
 
-      const list = data.slice(0, 10)
+      const list = data.slice(0, 10);
 
-      return json(list.map((u) => ({ id: u.id, name: u.name, email: u.email })))
+      return json(list.map((u) => ({ id: u.id, name: u.name, email: u.email })));
     },
-  })
+  });
